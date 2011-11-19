45ba8ff2792a90fe3f645dfd4cb2bb72421d8ccc
Raul Morales <raul.mh@gmail.com>
2011-11-19 18:38:34 +0100

    Use 4 spaces per indentation level (PEP 8)

--- cgmclass.py
+++ cgmclass.py
@@ -11,173 +11,173 @@ import ConfigParser
 import shlex
 
 def _remove(path):
-   # os.remove() wrapper
-   try:
-      os.remove(path)
-   except:
-      logging.exception("Cannot remove %s", path)
+    # os.remove() wrapper
+    try:
+        os.remove(path)
+    except:
+        logging.exception("Cannot remove %s", path)
 
 class CGMfile:
-   """
-   This class implements a CGM file identified by path and a set of methods
-   for converting to other formats. Format conversion is performed by
-   translators (external programs) as defined by developer.
+    """
+    This class implements a CGM file identified by path and a set of methods
+    for converting to other formats. Format conversion is performed by
+    translators (external programs) as defined by developer.
 
-   Translators may be defined per object, or globally. If globally defined,
-   translators are inherited by new CGMfile objects.
+    Translators may be defined per object, or globally. If globally defined,
+    translators are inherited by new CGMfile objects.
 
-   Filenames follow this rules:
-   - Case is ignored when looking for existing files (running on Windows)
-   - 'filename.ext' and 'trasp_filename.ext' are equivalent
-   - It is expected that the names of newly created files is 'filename.ext',
-      so be careful with translator definitions
-   """
-   _translator = {}
+    Filenames follow this rules:
+    - Case is ignored when looking for existing files (running on Windows)
+    - 'filename.ext' and 'trasp_filename.ext' are equivalent
+    - It is expected that the names of newly created files is 'filename.ext',
+       so be careful with translator definitions
+    """
+    _translator = {}
 
-   def __init__(self, path):
-      """
-      cgmclass.CGMfile(path) -> CGMfile Object
+    def __init__(self, path):
+        """
+        cgmclass.CGMfile(path) -> CGMfile Object
 
-      Argument is a path to a file with '.cgm' extension.
-      """
-      self._translator = self._translator.copy() # avoid modify class attr.
-      self._files = [] # list of temp files in working dir
+        Argument is a path to a file with '.cgm' extension.
+        """
+        self._translator = self._translator.copy() # avoid modify class attr.
+        self._files = [] # list of temp files in working dir
 
-      self.path = os.path.abspath(path)
-      self.dirname, self.filename = os.path.split(self.path)
-      self.name, ext = os.path.splitext(self.filename)
-      if not re.match('.cgm', ext, re.I) or not os.path.isfile(self.path):
-         logging.warning('%s: Invalid CGM file', self.path)
-         raise ValueError(path)
-      self.name = re.sub('^trasp_', '', self.name, flags=re.I)
+        self.path = os.path.abspath(path)
+        self.dirname, self.filename = os.path.split(self.path)
+        self.name, ext = os.path.splitext(self.filename)
+        if not re.match('.cgm', ext, re.I) or not os.path.isfile(self.path):
+            logging.warning('%s: Invalid CGM file', self.path)
+            raise ValueError(path)
+        self.name = re.sub('^trasp_', '', self.name, flags=re.I)
 
-      # Replace variables in command list
-      for k in self._translator.keys():
-         (retOk, commandlist) = self._translator[k]
-         self.addTranslator(k, retOk, commandlist)
+        # Replace variables in command list
+        for k in self._translator.keys():
+            (retOk, commandlist) = self._translator[k]
+            self.addTranslator(k, retOk, commandlist)
 
-   def __del__(self):
-      for f in self._files: _remove(f)
+    def __del__(self):
+        for f in self._files: _remove(f)
 
-   def __str__(self):
-      return self.path
+    def __str__(self):
+        return self.path
 
-   def _Get(self):
-      # Copy CGM file to working dir
-      if self.filename not in self._files:
-         try:
-            shutil.copyfile(self.path, self.filename)
-         except OSError:
-            logging.exception('Cannot copy "%s" in "%s"', self.path,
-               os.getcwd())
+    def _Get(self):
+        # Copy CGM file to working dir
+        if self.filename not in self._files:
+            try:
+                shutil.copyfile(self.path, self.filename)
+            except OSError:
+                logging.exception('Cannot copy "%s" in "%s"', self.path,
+                                  os.getcwd())
+                raise
+            self._files.append(self.filename)
+
+    def _Put(self, ext):
+        # Copy file name.ext in CGM original dir
+        f = self.name + ext
+        if f not in self._files:
+            logging.critical('CGMfile._Put(): File "%s" was not created', f)
+            return
+        try:
+            shutil.copyfile(f, os.path.join(self.dirname, f))
+        except OSError:
+            logging.exception('Cannot copy "%s" in "%s"', f, self.dirname)
             raise
-         self._files.append(self.filename)
 
-   def _Put(self, ext):
-      # Copy file name.ext in CGM original dir
-      f = self.name + ext
-      if f not in self._files:
-         logging.critical('CGMfile._Put(): File "%s" was not created', f)
-         return
-      try:
-         shutil.copyfile(f, os.path.join(self.dirname, f))
-      except OSError:
-         logging.exception('Cannot copy "%s" in "%s"', f, self.dirname)
-         raise
+    def _shouldTranslate(self, ext):
+        # Check if exists filename.ext in CGM dir
+        filename = self.name + ext
+        filetrasp = 'TRASP_' + filename
+        filename = os.path.join(self.dirname, filename)
+        filetrasp = os.path.join(self.dirname, filetrasp)
+        return not (os.path.isfile(filename) or os.path.isfile(filetrasp))
 
-   def _shouldTranslate(self, ext):
-      # Check if exists filename.ext in CGM dir
-      filename = self.name + ext
-      filetrasp = 'TRASP_' + filename
-      filename = os.path.join(self.dirname, filename)
-      filetrasp = os.path.join(self.dirname, filetrasp)
-      return not (os.path.isfile(filename) or os.path.isfile(filetrasp))
+    def Translate(self, ext):
+        """
+        Translate the CGM file using the proper translator, if it does not
+        exist a translated version file.
 
-   def Translate(self, ext):
-      """
-      Translate the CGM file using the proper translator, if it does not
-      exist a translated version file.
+        Temporal files are created in working directory
+        """
+        if ext not in self._translator.keys():
+            logging.critical('CGMfile.Translate(): No translator for "%s"', ext)
+            return
+        if not self._shouldTranslate(ext):
+            return
 
-      Temporal files are created in working directory
-      """
-      if ext not in self._translator.keys():
-         logging.critical('CGMfile.Translate(): No translator for "%s"', ext)
-         return
-      if not self._shouldTranslate(ext):
-         return
+        newfile = self.name + ext
+        logging.info('Translating: "%s" -> "%s"', self.filename, newfile)
+        self._Get()
+        (retOk, command) = self._translator[ext]
+        if retOk == subprocess.call(command):
+            self._files.append(newfile)
+            self._Put(ext)
+        else:
+            logging.error('%s: Translator returned an error', newfile)
 
-      newfile = self.name + ext
-      logging.info('Translating: "%s" -> "%s"', self.filename, newfile)
-      self._Get()
-      (retOk, command) = self._translator[ext]
-      if retOk == subprocess.call(command):
-         self._files.append(newfile)
-         self._Put(ext)
-      else:
-         logging.error('%s: Translator returned an error', newfile)
+    def TranslateAll(self):
+        """
+        Translate the CGM file using every translator defined
 
-   def TranslateAll(self):
-      """
-      Translate the CGM file using every translator defined
+        Exceptions are logged, but not raised
+        """
+        for ext in self._translator.keys():
+            try: self.Translate(ext)
+            except: # FIXME: Several exception MUST be raised
+                logging.exception('Error inside Translate("%s")', ext)
 
-      Exceptions are logged, but not raised
-      """
-      for ext in self._translator.keys():
-         try: self.Translate(ext)
-         except: # FIXME: Several exception MUST be raised
-            logging.exception('Error inside Translate("%s")', ext)
+    def addTranslator(self, ext, retOk, commandlist):
+        """
+        Add a translator definition for current object.
 
-   def addTranslator(self, ext, retOk, commandlist):
-      """
-      Add a translator definition for current object.
-
-      Arguments are like cgmclass.addTranslator() args.
-      """
-      command = [s.format(cgmfile=self.filename, name=self.name)
-                  for s in commandlist]
-      self._translator[ext] = (retOk, command)
+        Arguments are like cgmclass.addTranslator() args.
+        """
+        command = [s.format(cgmfile=self.filename, name=self.name)
+                   for s in commandlist]
+        self._translator[ext] = (retOk, command)
 
 def addTranslator(ext, retOk, commandlist):
-   """
-   Add a translator definition for new CGMfile objects.
+    """
+    Add a translator definition for new CGMfile objects.
 
-   Arguments description:
-   - ext: file extension like '.tif'
-   - retOk: return code for Ok translation (usually 0)
-   - commandlist: list of arguments used to run the external program.
+    Arguments description:
+    - ext: file extension like '.tif'
+    - retOk: return code for Ok translation (usually 0)
+    - commandlist: list of arguments used to run the external program.
       for example ['C:\\\\cgm2tif.exe', 'file.cgm', 'file.tif']
 
-   The following special strings can be used:
-   '{cgmfile}' is replaced by CGM file name
-   '{name}' is replaced by CGM file name without extension
-   """
-   logging.info('Setting "%s" translator', ext)
-   CGMfile._translator[ext] = (retOk, commandlist)
+    The following special strings can be used:
+    '{cgmfile}' is replaced by CGM file name
+    '{name}' is replaced by CGM file name without extension
+    """
+    logging.info('Setting "%s" translator', ext)
+    CGMfile._translator[ext] = (retOk, commandlist)
 
 def addTranslatorsFromFile(filename):
-   """
-   Add a set of translators definitions as defined inside file.
+    """
+    Add a set of translators definitions as defined inside file.
 
-   File has INI-like format, with file extensions as sections and 3 key=value
-   pairs: translator, args, and success.
-   - Translator is a Win32 app path
-   - Args is a list of arguments passed to translator, separated by
-     whitespaces. You can use quotes if needed.
-   - Success is the exit status when all went well
+    File has INI-like format, with file extensions as sections and 3 key=value
+    pairs: translator, args, and success.
+    - Translator is a Win32 app path
+    - Args is a list of arguments passed to translator, separated by
+      whitespaces. You can use quotes if needed.
+    - Success is the exit status when all went well
 
-   [.ext]
-   translator = C:\\Program Files\\cgm2ext.exe
-   args = {cgmfile} {name}.ext
-   success = 0
-   """
-   logging.info('Reading translators from "%s"', filename)
-   config = ConfigParser.RawConfigParser()
-   with open(filename) as cfgfile:
-      config.readfp(cfgfile)
+    [.ext]
+    translator = C:\\Program Files\\cgm2ext.exe
+    args = {cgmfile} {name}.ext
+    success = 0
+    """
+    logging.info('Reading translators from "%s"', filename)
+    config = ConfigParser.RawConfigParser()
+    with open(filename) as cfgfile:
+        config.readfp(cfgfile)
 
-   for ext in config.sections():
-      retOk = config.getint(ext, 'success')
-      args = config.get(ext, 'args')
-      command = shlex.split(args)
-      command.insert(0, config.get(ext, 'translator'))
-      addTranslator(ext, retOk, command)
+    for ext in config.sections():
+        retOk = config.getint(ext, 'success')
+        args = config.get(ext, 'args')
+        command = shlex.split(args)
+        command.insert(0, config.get(ext, 'translator'))
+        addTranslator(ext, retOk, command)
--- cgmlog.py
+++ cgmlog.py
@@ -2,65 +2,64 @@
 Logging module for CGM-Manager.
 """
 
-import logging, logging.handlers
+import logging
+import logging.handlers
 import time
 import os.path
 
 # There is no logging handler with needed features, so we write one
 class DailyRotatingFileHandler(logging.handlers.BaseRotatingHandler):
-   """
-   Handler for daily rotating log files. After midnight it writes log entries
-   into a new log file so you get a logfile a day.
-   """
-   # Interesting inherited attributes:
-   # self.stream: File like Object
-   # self.baseFilename = os.path.abspath(logfile)
-   # self.mode: Mode used to open log file
+    """
+    Handler for daily rotating log files. After midnight it writes log entries
+    into a new log file so you get a logfile a day.
+    """
+    # Interesting inherited attributes:
+    # self.stream: File like Object
+    # self.baseFilename = os.path.abspath(logfile)
+    # self.mode: Mode used to open log file
 
-   def __init__(self, fileprefix, encoding=None):
-      """
-      Open the file prefix.date.log and use it as destination for logging.
+    def __init__(self, fileprefix, encoding=None):
+        """
+        Open the file prefix.date.log and use it as destination for logging.
 
-      Rollover occurs when file date mismatch with current date.
-      """
-      self.__date =  time.strftime("%Y-%m-%d")
-      self.__prefix = fileprefix
-      filename = "{0}.{1}.log".format(self.__prefix, self.__date)
-      logging.handlers.BaseRotatingHandler.__init__(self, filename, 'a',
-                                                    encoding)
+        Rollover occurs when file date mismatch with current date.
+        """
+        self.__date =  time.strftime("%Y-%m-%d")
+        self.__prefix = fileprefix
+        filename = "{0}.{1}.log".format(self.__prefix, self.__date)
+        logging.handlers.BaseRotatingHandler.__init__(self, filename, 'a', encoding)
 
-   def shouldRollover(self, record):
-      'Check if date changed since last write'
-      return time.strftime("%Y-%m-%d") != self.__date
+    def shouldRollover(self, record):
+        'Check if date changed since last write'
+        return time.strftime("%Y-%m-%d") != self.__date
 
-   def doRollover(self):
-      'Close log file and open a new one based on date'
-      self.__date = time.strftime('%Y-%m-%d')
-      self.stream.close()
-      self.stream = None
-      # Set new baseFilename but do not open stream,
-      # we expect parent classes do it when needed
-      dirname = os.path.dirname(self.baseFilename)
-      filename = '{0}.{1}.log'.format(self.__prefix, self.__date)
-      self.baseFilename = os.path.join(dirname, filename)
-      # TODO: We can gzip old log file
+    def doRollover(self):
+        'Close log file and open a new one based on date'
+        self.__date = time.strftime('%Y-%m-%d')
+        self.stream.close()
+        self.stream = None
+        # Set new baseFilename but do not open stream,
+        # we expect parent classes do it when needed
+        dirname = os.path.dirname(self.baseFilename)
+        filename = '{0}.{1}.log'.format(self.__prefix, self.__date)
+        self.baseFilename = os.path.join(dirname, filename)
+        # TODO: We can gzip old log file
 
 def basicConfig(fileprefix, level=logging.INFO):
-   """
-   Wrapper to logging.basicConfig()
+    """
+    Wrapper to logging.basicConfig()
 
-   Add a DailyRotatingFileHandler to the root logger using fileprefix as
-   a prefix for filename. Filename is calculated as <prefix>.<date>.log
-   and is created a log file a day.
-   """
-   fmt = logging.Formatter('%(asctime)s - %(levelname)-10s - %(message)s',
-                           '%H:%M:%S')
+    Add a DailyRotatingFileHandler to the root logger using fileprefix as
+    a prefix for filename. Filename is calculated as <prefix>.<date>.log
+    and is created a log file a day.
+    """
+    fmt = logging.Formatter('%(asctime)s - %(levelname)-10s - %(message)s',
+                            '%H:%M:%S')
 
-   hl = DailyRotatingFileHandler(fileprefix)
-   hl.setLevel(level)
-   hl.setFormatter(fmt)
+    hl = DailyRotatingFileHandler(fileprefix)
+    hl.setLevel(level)
+    hl.setFormatter(fmt)
 
-   logger = logging.getLogger('')
-   logger.setLevel(level)
-   logger.addHandler(hl)
-
+    logger = logging.getLogger('')
+    logger.setLevel(level)
+    logger.addHandler(hl)
