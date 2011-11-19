af0964677ec1ea0be2af851602b520ccc0574954
Raul Morales <raul.mh@gmail.com>
2011-11-19 15:00:03 +0100

    Almost functional module cgmman.py

--- cgmman.py
+++ cgmman.py
@@ -6,12 +6,28 @@ import os
 import sys
 import ConfigParser
 import logging
+import time
 
 import cgmlog
 import cgmclass
 
 srcdir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
+def ExtFilter(path, extfilter):
+    name, ext = os.path.splitext(path)
+    return re.match(extfilter, ext, re.I) is not None
+
+def TranslateDir(dirname):
+    cgmlist = [ os.path.join(dirname, f)
+        for f in os.listdir(dirname)
+        if ExtFilter(f, '.cgm') ]
+    for f in cgmlist:
+        try:
+            cgm = cgmclass.CGMfile(f)
+        except ValueError:
+            logging.warning('Invalid CGM file: %s', f)
+        else:
+            cgm.TranslateAll()
 
 if __name__ == '__main__':
     conf = ConfigParser.RawConfigParser()
@@ -28,4 +44,5 @@ if __name__ == '__main__':
     cgmclass.addTranslatorsFromFile(os.path.join(srcdir, 'Translators.ini'))
     
     while not os.path.isfile('stop'):
-        pass
+        TranslateDir(cgmdir)
+        time.sleep(sleeptime)
